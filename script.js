8ea4e65d908452810eebfea597d8452cde66811f
supernovamiyu <rojascamposyinet1101@gmail.com>
2024-11-05 17:28:51 -0500

    Función para cerrar el menú para celulares.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
     //Ver el menú
     document.body.classList.toggle("show-mobile-menu")
-})
+});
+
+// Cerrar el menú cuando el botón de cerrado es clicado
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
