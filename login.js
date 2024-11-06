e586f1225a6e2f5c5e16b0759dc1cf852f5fe0f9
supernovamiyu <rojascamposyinet1101@gmail.com>
2024-11-06 17:38:45 -0500

    Se realizó la función para hacer el login del usuario en la página web de SELFCARE.

--- login.js
+++ login.js
@@ -1,13 +1,18 @@
-const loginForm = document.querySelector('#login-form')
+const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-    const users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = users.find(user => user.email === email && user.password === password)
+    event.preventDefault();
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const validUser = users.find(user => user.email === email && user.password === password);
+    
     if (!validUser) {
-        return alert("Usuario y/o contraseña incorrectos")
+        return alert("Usuario y/o contraseña incorrectos");
     }
+
+    // Almacenar el nombre del usuario en localStorage
+    localStorage.setItem('loggedInUser', JSON.stringify(validUser));
+
     alert(`Bienvenido, ${validUser.name}`);
-    window.location.href = 'index.html'
-})
+    window.location.href = 'index-usuario.html'; // Redirigir a la página principal
+});
